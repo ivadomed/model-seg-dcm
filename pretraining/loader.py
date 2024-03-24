a3f6f52165278ce29633a973bce80bf42dc75eb8
Naga Karthik <emvnagakarthik@gmail.com>
2024-03-23 23:39:39 -0400

    add script for dataloading

---
+++ pretraining/loader.py
@@ -0,0 +1,42 @@
+import os
+
+from monai.data import (DataLoader, Dataset, list_data_collate, DistributedSampler,
+                        CacheDataset, load_decathlon_datalist)
+
+from transforms import train_transforms, val_transforms
+
+
+def load_data(datalist_path, train_batch_size, val_batch_size, num_workers=8, use_distributed=False, 
+              crop_size=(64, 192, 320), patch_size=(64, 64, 64), device="cuda", task="pretraining"):
+    """
+    Return train and val dataloaders from datalist json file
+    """
+
+    # TODO: accommodate multiple datalists
+
+    train_datalist = load_decathlon_datalist(data_list_file_path=datalist_path, data_list_key="training")
+    val_datalist = load_decathlon_datalist(data_list_file_path=datalist_path, data_list_key="validation")
+
+    train_tfs = train_transforms(crop_size, patch_size, device=device, type=task)
+    val_tfs = val_transforms(crop_size, type=task)
+
+    # training dataset
+    train_ds = CacheDataset(data=train_datalist, transform=train_tfs, cache_rate=0.5, num_workers=8)
+    # validation dataset
+    val_ds = CacheDataset(data=val_datalist, transform=val_tfs, cache_rate=0.25, num_workers=8)
+
+    if use_distributed:
+        train_sampler = DistributedSampler(dataset=train_ds, even_divisible=True, shuffle=True)
+        val_sampler = DistributedSampler(dataset=val_ds, even_divisible=True, shuffle=False)
+    else:
+        train_sampler = None
+        val_sampler = None
+
+    # training dataloader    
+    train_loader = DataLoader(train_ds, batch_size=train_batch_size, shuffle=False, num_workers=num_workers, 
+                            pin_memory=False, sampler=train_sampler, persistent_workers=True, drop_last=False)
+    # validation dataloader
+    val_loader = DataLoader(val_ds, batch_size=val_batch_size, shuffle=False, num_workers=num_workers,
+                            pin_memory=False, sampler=val_sampler, persistent_workers=True, drop_last=False)
+
+    return train_loader, val_loader
