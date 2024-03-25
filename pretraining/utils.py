eadfc4797f0820fb1fb9524d6a375c2e07bc0a33
Naga Karthik <emvnagakarthik@gmail.com>
2024-03-24 22:43:58 -0400

    add utils file

---
+++ pretraining/utils.py
@@ -0,0 +1,9 @@
+
+
+def dice_score(prediction, groundtruth):
+    smooth = 1.
+    numer = (prediction * groundtruth).sum()
+    denor = (prediction + groundtruth).sum()
+    # loss = (2 * numer + self.smooth) / (denor + self.smooth)
+    dice = (2 * numer + smooth) / (denor + smooth)
+    return dice
