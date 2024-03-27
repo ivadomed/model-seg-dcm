ddca389047f2d63022bb231949f0f16085b0997e
valosekj <jan.valosek@upol.cz>
2024-03-27 07:47:17 -0400

    PEP8 formatting

--- pretraining/transforms.py
+++ pretraining/transforms.py
@@ -1,14 +1,12 @@
-
 import numpy as np
 import monai.transforms as transforms
 
 
 def train_transforms(crop_size, patch_size, device="cuda", mode="pretraining"):
-
     if mode == "pretraining":
         # NOTE: the pre-trainining is done for SC segmentation
         all_keys = ["image", "label_sc"]
-        
+
         monai_transforms = [
             # pre-processing    
             transforms.LoadImaged(keys=all_keys, image_only=False),
@@ -21,38 +19,42 @@ def train_transforms(crop_size, patch_size, device="cuda", mode="pretraining"):
             transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size),
             # convert the data to Tensor without meta, move to GPU and cache it to avoid CPU -> GPU sync in every epoch
             transforms.transforms.EnsureTyped(keys=all_keys, device=device, track_meta=False),
-            
+
             # sample patches from cropped image
             transforms.RandCropByPosNegLabeld(keys=all_keys,
-                label_key="label_sc",       # cropping around the SC
-                spatial_size=patch_size,
-                pos=1, neg=0,
-                num_samples=4,  # if num_samples=4, then 4 samples/image are randomly generated
-                image_key="image", image_threshold=0),
-            
+                                              label_key="label_sc",  # cropping around the SC
+                                              spatial_size=patch_size,
+                                              pos=1, neg=0,
+                                              # if num_samples=4, then 4 samples/image are randomly generated
+                                              num_samples=4,
+                                              image_key="image", image_threshold=0),
+
             # data-augmentation
             # NOTE: we use heavily-deforming transformations for SC segmentation
             transforms.RandAffined(keys=all_keys, mode=(2, 1), prob=0.9,
-                        rotate_range=(-20. / 360 * 2. * np.pi, 20. / 360 * 2. * np.pi),    # monai expects in radians 
-                        scale_range=(-0.2, 0.2),   
-                        translate_range=(-0.1, 0.1)),
+                                   # monai expects in radians
+                                   rotate_range=(-20. / 360 * 2. * np.pi, 20. / 360 * 2. * np.pi),
+                                   scale_range=(-0.2, 0.2),
+                                   translate_range=(-0.1, 0.1)),
             transforms.Rand3DElasticd(keys=all_keys, prob=0.5,
-                        sigma_range=(3.5, 5.5), 
-                        magnitude_range=(25., 35.)),
+                                      sigma_range=(3.5, 5.5),
+                                      magnitude_range=(25., 35.)),
             transforms.RandSimulateLowResolutiond(keys=["image"], zoom_range=(0.5, 1.0), prob=0.25),
-            transforms.RandAdjustContrastd(keys=["image"], gamma=(0.5, 3.), prob=0.5),    # this is monai's RandomGamma
+            transforms.RandAdjustContrastd(keys=["image"], gamma=(0.5, 3.), prob=0.5),  # this is monai's RandomGamma
             transforms.RandBiasFieldd(keys=["image"], coeff_range=(0.0, 0.5), degree=3, prob=0.3),
             transforms.RandGaussianNoised(keys=["image"], mean=0.0, std=0.1, prob=0.1),
-            transforms.RandGaussianSmoothd(keys=["image"], sigma_x=(0., 2.), sigma_y=(0., 2.), sigma_z=(0., 2.0), prob=0.3),
-            transforms.RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.15),  # this is nnUNet's BrightnessMultiplicativeTransform
-            transforms.RandFlipd(keys=all_keys, prob=0.3,),
+            transforms.RandGaussianSmoothd(keys=["image"], sigma_x=(0., 2.), sigma_y=(0., 2.), sigma_z=(0., 2.0),
+                                           prob=0.3),
+            transforms.RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.15),
+            # this is nnUNet's BrightnessMultiplicativeTransform
+            transforms.RandFlipd(keys=all_keys, prob=0.3, ),
         ]
     elif mode == "finetuning":
 
         # NOTE: the fine-tuning is done for lesion segmentation
         all_keys = ["image", "label_sc", "label_lesion"]
 
-        monai_transforms = [    
+        monai_transforms = [
             # pre-processing
             transforms.LoadImaged(keys=all_keys),
             transforms.EnsureChannelFirstd(keys=all_keys),
@@ -60,7 +62,7 @@ def train_transforms(crop_size, patch_size, device="cuda", mode="pretraining"):
             # NOTE: spine interpolation with order=2 is spline, order=1 is linear
             transforms.Spacingd(keys=all_keys, pixdim=(1.0, 1.0, 1.0), mode=(2, 1, 1)),
             transforms.NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
-            transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size,),
+            transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size, ),
             # convert the data to Tensor without meta, move to GPU and cache it to avoid CPU -> GPU sync in every epoch
             transforms.EnsureTyped(keys=all_keys, device=device, track_meta=False),
             # data-augmentation
@@ -70,32 +72,37 @@ def train_transforms(crop_size, patch_size, device="cuda", mode="pretraining"):
             #             translate_range=(-0.1, 0.1)),
             # transforms.Rand3DElasticd(keys=all_keys, prob=0.5, sigma_range=(3.5, 5.5), magnitude_range=(25., 35.)),
             transforms.RandSimulateLowResolutiond(keys=["image"], zoom_range=(0.5, 1.0), prob=0.25),
-            transforms.RandAdjustContrastd(keys=["image"], gamma=(0.5, 3.), prob=0.25),    # this is monai's RandomGamma
+            transforms.RandAdjustContrastd(keys=["image"], gamma=(0.5, 3.), prob=0.25),  # this is monai's RandomGamma
             # transforms.RandBiasFieldd(keys=["image"], coeff_range=(0.0, 0.5), degree=3, prob=0.3),
             # transforms.RandGaussianNoised(keys=["image"], mean=0.0, std=0.1, prob=0.1),
-            transforms.RandGaussianSmoothd(keys=["image"], sigma_x=(0., 2.), sigma_y=(0., 2.), sigma_z=(0., 2.0), prob=0.3),
-            transforms.RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.15),  # this is nnUNet's BrightnessMultiplicativeTransform
-            transforms.RandFlipd(keys=all_keys, prob=0.3,),
+            transforms.RandGaussianSmoothd(keys=["image"], sigma_x=(0., 2.), sigma_y=(0., 2.), sigma_z=(0., 2.0),
+                                           prob=0.3),
+            transforms.RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.15),
+            # this is nnUNet's BrightnessMultiplicativeTransform
+            transforms.RandFlipd(keys=all_keys, prob=0.3, ),
         ]
     else:
         raise ValueError("Invalid type: {}. Choices: [pretraining, finetuning]".format(type))
 
-    return transforms.Compose(monai_transforms) 
+    return transforms.Compose(monai_transforms)
+
 
 def inference_transforms(crop_size, lbl_key="label"):
     return transforms.Compose([
-            transforms.LoadImaged(keys=["image", lbl_key], image_only=False),
-            transforms.EnsureChannelFirstd(keys=["image", lbl_key]),
-            # CropForegroundd(keys=["image", lbl_key], source_key="image"),
-            transforms.Orientationd(keys=["image", lbl_key], axcodes="RPI"),
-            transforms.Spacingd(keys=["image", lbl_key], pixdim=(1.0, 1.0, 1.0), mode=(2, 1)), # mode=("bilinear", "bilinear"),),
-            transforms.ResizeWithPadOrCropd(keys=["image", lbl_key], spatial_size=crop_size,),
-            transforms.DivisiblePadd(keys=["image", lbl_key], k=2**5),   # pad inputs to ensure divisibility by no. of layers nnUNet has (5)
-            transforms.NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
-        ])
+        transforms.LoadImaged(keys=["image", lbl_key], image_only=False),
+        transforms.EnsureChannelFirstd(keys=["image", lbl_key]),
+        # CropForegroundd(keys=["image", lbl_key], source_key="image"),
+        transforms.Orientationd(keys=["image", lbl_key], axcodes="RPI"),
+        transforms.Spacingd(keys=["image", lbl_key], pixdim=(1.0, 1.0, 1.0), mode=(2, 1)),
+        # mode=("bilinear", "bilinear"),),
+        transforms.ResizeWithPadOrCropd(keys=["image", lbl_key], spatial_size=crop_size, ),
+        transforms.DivisiblePadd(keys=["image", lbl_key], k=2 ** 5),
+        # pad inputs to ensure divisibility by no. of layers nnUNet has (5)
+        transforms.NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
+    ])
+
 
 def val_transforms(crop_size, mode="pretraining"):
-
     if mode == "pretraining":
         all_keys = ["image", "label_sc"]
 
@@ -105,7 +112,7 @@ def val_transforms(crop_size, mode="pretraining"):
             transforms.Orientationd(keys=all_keys, axcodes="RPI"),
             transforms.Spacingd(keys=all_keys, pixdim=(1.0, 1.0, 1.0), mode=(2, 1)),
             transforms.NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
-            transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size,),
+            transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size, ),
         ])
     elif mode == "finetuning":
         all_keys = ["image", "label_sc", "label_lesion"]
@@ -115,7 +122,7 @@ def val_transforms(crop_size, mode="pretraining"):
             transforms.Orientationd(keys=all_keys, axcodes="RPI"),
             transforms.Spacingd(keys=all_keys, pixdim=(1.0, 1.0, 1.0), mode=(2, 1, 1)),
             transforms.NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
-            transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size,),
+            transforms.ResizeWithPadOrCropd(keys=all_keys, spatial_size=crop_size, ),
         ])
-    else :
+    else:
         raise ValueError("Invalid type: {}. Choices: [pretraining, finetuning]".format(type))
